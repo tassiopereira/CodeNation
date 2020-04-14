a3f7d97717df6b2dc47cf9e69786b408df705729
tassiopereira <44480617+tassiopereira@users.noreply.github.com>
2020-04-14 14:51:03 -0300

    Add files via upload

---
+++ Helper/HashSha1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CodeNation_Cript.Helper
+{
+    public static class HashSha1
+    {
+        public static string Hash(string input)
+        {
+            var hash = new SHA1Managed().ComputeHash(Encoding.UTF8.GetBytes(input));
+            return string.Concat(hash.Select(b => b.ToString("x2")));
+        }
+    }
+}
---
+++ Model/Answer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeNation_Cript.Model
+{
+    public class Answer
+	{
+		public int numero_casas { get; set; }
+		public string token { get; set; }
+		public string cifrado { get; set; }
+		public string decifrado { get; set; }
+		public string resumo_criptografico { get; set; }
+	}
+}
